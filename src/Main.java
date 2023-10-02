b83fc57a95bbb77f3d46b8db8920e4310db580ec
viktordiktor <eversum69@gmail.com>
2023-10-02 21:14:14 +0300

    user1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void greeting(){
+        System.out.println("Hello my name is Viktor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
